e26e70d09cdcffd4a2f4ac5997b00b3270555aa1
Andres Bryant <bmejiadeveloper2@gmail.com>
2026-02-18 22:47:33 -0500

    feat(logger): configure logger to write to stdout and filesystem

--- src/shared/logger/logger.ts
+++ src/shared/logger/logger.ts
@@ -2,8 +2,6 @@ import pino from 'pino';
 import os from 'os';
 import path from 'path';
 
-
-
 const isDev = process.env.NODE_ENV !== 'production';
 const hostname = os.hostname().toLowerCase();
 const pid = process.pid;
@@ -20,19 +18,19 @@ const targets: pino.TransportTargetOptions[] = [
         } : { destination: 1 }, // Explicit stdout in production
         level: process.env.LOG_LEVEL || 'info',
     },
-    //  {
-    //     target: 'pino-roll',
-    //     options: { 
-    //         file: logPath,
-    //         frequency: 2 * 24 * 60 * 60 * 1000, // ✅ 2 days in milliseconds
-    //         limit: {
-    //             count: 2 // Keep 2 rotated files
-    //         },
-    //         mkdir: true,
-    //         sync: isDev
-    //     },
-    //     level: process.env.LOG_LEVEL || 'info',
-    // }
+     {
+        target: 'pino-roll',
+        options: { 
+            file: logPath,
+            frequency: 2 * 24 * 60 * 60 * 1000, // ✅ 2 days in milliseconds
+            limit: {
+                count: 2 // Keep 2 rotated files
+            },
+            mkdir: true,
+            sync: isDev
+        },
+        level: process.env.LOG_LEVEL || 'info',
+    }
 ];
 
 export const logger = pino({
@@ -40,8 +38,6 @@ export const logger = pino({
     base: {
         w_id: `${hostname}-${pid}`,
     },
-    formatters: {
-        level: (label) => ({ level: label.toUpperCase() }),
-    },
     timestamp: () => `,"ts_iso":"${new Date().toISOString()}","ts_human":"${new Date().toLocaleString()}"`,
 }, pino.transport({ targets }));
+
