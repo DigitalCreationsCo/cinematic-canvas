68d23aa7bd506629c7b9fe9930c14c87fb35a681
Bryant Mejia <bmejia220@gmail.com>
2026-03-13 11:21:10 -0400

    style: make footer links fit content width on md views

--- website/components/navigation/footer.tsx
+++ website/components/navigation/footer.tsx
@@ -5,7 +5,7 @@ import { Settings } from '#/types/settings'
 
 export function Footer() {
   return (
-    <footer className="w-full relative z-10 glass-brick border-t border-border/20 flex flex-col text-sm text-foreground">
+    <footer className="w-full relative z-10 flex flex-col text-sm text-foreground">
       {/* {Settings.branding !== false && (
         <div className="hidden items-center md:block">
           <Link
@@ -28,19 +28,18 @@ export function Footer() {
 </div>
       ) } */}
 
-      <div className="py-12 px-4 lg:px-8 w-fit md:w-6xl mx-auto">
-        <div className="flex flex-col-reverse md:flex-row-reverse justify-left md:justify-between md:items-center text-xs text-muted-foreground">
+      <div className="py-12 px-4 lg:px-8 w-fit md:max-w-6xl mx-auto">
+        <div className="mx-auto flex flex-col-reverse md:flex-row-reverse justify-center md:items-center text-xs text-muted-foreground gap-4">
           <p className="md:mx-0">&copy; { new Date().getFullYear() } Cinematic Canvas.</p>
-          <div className="mx-auto md:mx-0 grid grid-flow-row grid-cols-3 auto-cols-max gap-4 mb-4 md:mb-0 md:gap-2 items-center">
-            <Link href="/updates" className="uppercase tracking-widest text-muted-foreground hover:text-foreground transition-colors">Updates</Link>
-            <Link href="/docs" className="uppercase tracking-widest text-muted-foreground hover:text-foreground transition-colors">Docs</Link>
-            <Link href="/examples" className="uppercase tracking-widest text-muted-foreground hover:text-foreground transition-colors">Examples</Link>
+          <div className="grid grid-flow-row md:grid-flow-col auto-cols-max gap-4 mb-4 md:mb-0 items-center">
+            <Link href="/docs" className="tracking-widest text-muted-foreground hover:text-foreground transition-colors">Docs</Link>
+            <Link href="/examples" className="tracking-widest text-muted-foreground hover:text-foreground transition-colors">Gallery</Link>
+            <Link href="/updates" className="tracking-widest text-muted-foreground hover:text-foreground transition-colors">Updates</Link>
             <Link href="https://github.com/AndresB/cinematic-canvas" className="hover:text-foreground transition-colors">GitHub</Link>
-            <Link href="#" className="hover:text-foreground transition-colors">Twitter</Link>
-            <Link href="#" className="hover:text-foreground transition-colors">Discord</Link>
           </div>
         </div>
       </div>
+
     </footer>
   )
 }
