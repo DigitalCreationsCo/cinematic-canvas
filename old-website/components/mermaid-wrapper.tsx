05db17d35ba9b060d33947eb52da98402825412a
Bryant Mejia <bmejia220@gmail.com>
2026-05-10 20:27:13 -0400

    chore: deleted old next-js website

--- old-website/components/mermaid-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import MermaidClient from '#/components/markdown/mermaid'
-
-export default function MermaidWrapper(props: { chart: string; className?: string }) {
-  return <MermaidClient {...props} />
-}
